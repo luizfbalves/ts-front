fc509c643d5c60619d7e3a9e6438f56c435e976d
luizzbanndera <31023594+LuizzBanndera@users.noreply.github.com>
2020-10-17 00:30:00 -0300

    Update home.tsx

--- src/views/content/home.tsx
+++ src/views/content/home.tsx
@@ -1,10 +1,20 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+import { CircularProgress } from '@material-ui/core'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%; 
+  width: 100%; 
+`
 
 export default function Component() {
 
   return (
-    <Fragment>
-      Home
-    </Fragment>
+    <Container>
+      <CircularProgress />
+    </Container>
   )
 }
